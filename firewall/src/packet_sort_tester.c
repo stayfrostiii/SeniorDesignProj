2508902e850e7a2cde5d4882c23d03e3c9124cc3
stayfrostiii <jowu.32@gmail.com>
2025-05-12 02:19:17 -0700

    making packet_sort_tester.c use 9 message packs

--- firewall/src/packet_sort_tester.c
+++ firewall/src/packet_sort_tester.c
@@ -22,18 +22,36 @@ int main()
     filter_packets("./logs/packets01.msgpack", prot_filter, prot, &protCount);
     filter_packets("./logs/packets02.msgpack", prot_filter, prot, &protCount);
     filter_packets("./logs/packets03.msgpack", prot_filter, prot, &protCount);
+    filter_packets("./logs/packets04.msgpack", prot_filter, prot, &protCount);
+    filter_packets("./logs/packets05.msgpack", prot_filter, prot, &protCount);
+    filter_packets("./logs/packets06.msgpack", prot_filter, prot, &protCount);
+    filter_packets("./logs/packets07.msgpack", prot_filter, prot, &protCount);
+    filter_packets("./logs/packets08.msgpack", prot_filter, prot, &protCount);
+    filter_packets("./logs/packets09.msgpack", prot_filter, prot, &protCount);
     printf("\nNumber of %s Protocols: %d\n", prot, protCount);
 
     filter_packets("./logs/packets00.msgpack", dest_ipFilter, dest_ip, &dest_ipCount);
     filter_packets("./logs/packets01.msgpack", dest_ipFilter, dest_ip, &dest_ipCount);
     filter_packets("./logs/packets02.msgpack", dest_ipFilter, dest_ip, &dest_ipCount);
     filter_packets("./logs/packets03.msgpack", dest_ipFilter, dest_ip, &dest_ipCount);
+    filter_packets("./logs/packets04.msgpack", dest_ipFilter, dest_ip, &dest_ipCount);
+    filter_packets("./logs/packets05.msgpack", dest_ipFilter, dest_ip, &dest_ipCount);
+    filter_packets("./logs/packets06.msgpack", dest_ipFilter, dest_ip, &dest_ipCount);
+    filter_packets("./logs/packets07.msgpack", dest_ipFilter, dest_ip, &dest_ipCount);
+    filter_packets("./logs/packets08.msgpack", dest_ipFilter, dest_ip, &dest_ipCount);
+    filter_packets("./logs/packets09.msgpack", dest_ipFilter, dest_ip, &dest_ipCount);
     printf("\nNumber of %s Destination IPs: %d\n", dest_ip, dest_ipCount);
 
     filter_packets("./logs/packets00.msgpack", src_ipFilter, src_ip, &src_ipCount);
     filter_packets("./logs/packets01.msgpack", src_ipFilter, src_ip, &src_ipCount);
     filter_packets("./logs/packets02.msgpack", src_ipFilter, src_ip, &src_ipCount);
     filter_packets("./logs/packets03.msgpack", src_ipFilter, src_ip, &src_ipCount);
+    filter_packets("./logs/packets04.msgpack", src_ipFilter, src_ip, &src_ipCount);
+    filter_packets("./logs/packets05.msgpack", src_ipFilter, src_ip, &src_ipCount);
+    filter_packets("./logs/packets06.msgpack", src_ipFilter, src_ip, &src_ipCount);
+    filter_packets("./logs/packets07.msgpack", src_ipFilter, src_ip, &src_ipCount);
+    filter_packets("./logs/packets08.msgpack", src_ipFilter, src_ip, &src_ipCount);
+    filter_packets("./logs/packets09.msgpack", src_ipFilter, src_ip, &src_ipCount);
     printf("\nNumber of %s Source IPs: %d\n", src_ip, src_ipCount);
 
     return 0;
