e528e86adbded567fd9f544d4752f71d0529597a
stayfrostiii <jowu.32@gmail.com>
2025-05-14 23:14:05 -0700

    change parameters for printing packets in packet_sort_tester.c

--- firewall/src/packet_sort_tester.c
+++ firewall/src/packet_sort_tester.c
@@ -10,7 +10,7 @@ int main()
 {
     uint64_t protCount = 0;
     char* prot_filter = "protocol";
-    char* prot = "TCP";
+    char* prot = "UDP";
 
     uint64_t dest_ipCount = 0;
     char* dest_ipFilter = "dst_ip";
