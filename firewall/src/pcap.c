295d2ca55e8e78b2826d47715f92143b78c619ec
stayfrostiii <jowu.32@gmail.com>
2025-05-14 22:57:47 -0700

    testing pbuf_size

--- firewall/src/pcap.c
+++ firewall/src/pcap.c
@@ -246,7 +246,7 @@ void packet_handler(unsigned char *user_data, const struct pcap_pkthdr *pkthdr, 
     
     pthread_mutex_unlock(&pbuf_lock);  // Always unlock
     
-    if (pbuf_size > 10000)
+    if (pbuf_size > 100)
     {
         pthread_cond_signal(&pbuf_cond);  // Notify the waiting thread
     }
@@ -268,7 +268,7 @@ void *pb_thread(void* args)
         pthread_mutex_lock(&pbuf_lock);
 
         // Wait until the condition is met (pbuf_size > 10000)
-        while (pbuf_size <= 10000)
+        while (pbuf_size <= 100)
         {
             pthread_cond_wait(&pbuf_cond, &pbuf_lock);
         }
