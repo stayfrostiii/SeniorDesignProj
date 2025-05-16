6ff512f07f342c2c46f7689b6fd2fc81f48e20e9
stayfrostiii <jowu.32@gmail.com>
2025-05-16 01:47:38 -0700

    testing packet display speed

--- firewall/src/pcap.c
+++ firewall/src/pcap.c
@@ -518,6 +518,7 @@ int main()
      
     /* Take the first device */
     dev = allDevs;
+    dev = dev->next; // Select ethernet port going to host from pi
     printf("%s\n", dev->name);
 
     /* Open capturing sesh */
