0924754af7fce870e9f9e209da9699475fed7db2
stayfrostiii <jowu.32@gmail.com>
2025-05-16 02:33:33 -0700

    testing libnetfilter_queue

--- firewall/src/libnet.c
+++ firewall/src/libnet.c
@@ -34,61 +34,6 @@
 //     return nfq_set_verdict(qh, id, NF_ACCEPT, 0, NULL);
 // }
 
-// int main()
-// {
-//     struct nfq_handle *h;
-//     struct nfq_q_handle *qh;
-//     int fd;
-//     int rv;
-//     char buf[4096] __attribute__ ((aligned));
-
-//     printf("Opening library handle\n");
-//     h = nfq_open();
-//     if (!h) {
-//         fprintf(stderr, "Error during nfq_open()\n");
-//         exit(1);
-//     }
-
-//     printf("Unbinding existing nf_queue handler for AF_INET (if any)\n");
-//     if (nfq_unbind_pf(h, AF_INET) < 0) {
-//         fprintf(stderr, "Error during nfq_unbind_pf()\n");
-//         exit(1);
-//     }
-
-//     printf("Binding nfnetlink_queue as nf_queue handler for AF_INET\n");
-//     if (nfq_bind_pf(h, AF_INET) < 0) {
-//         fprintf(stderr, "Error during nfq_bind_pf()\n");
-//         exit(1);
-//     }
-
-//     printf("Binding this socket to queue '0'\n");
-//     qh = nfq_create_queue(h, 0, &cb, NULL);
-//     if (!qh) {
-//         fprintf(stderr, "Error during nfq_create_queue()\n");
-//         exit(1);
-//     }
-
-//     printf("Setting copy_packet mode\n");
-//     if (nfq_set_mode(qh, NFQNL_COPY_PACKET, 0xffff) < 0) {
-//         fprintf(stderr, "Can't set packet_copy mode\n");
-//         exit(1);
-//     }
-
-//     fd = nfq_fd(h);
-
-//     while ((rv = recv(fd, buf, sizeof(buf), 0)) && rv >= 0) {
-//         nfq_handle_packet(h, buf, rv);
-//     }
-
-//     printf("Unbinding from queue 0\n");
-//     nfq_destroy_queue(qh);
-
-//     printf("Closing library handle\n");
-//     nfq_close(h);
-
-//     return 0;
-// }
-
 #include <stdio.h>
 #include <stdint.h>
 #include <arpa/inet.h>
@@ -222,4 +167,59 @@ static int cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg,
 
     // For all other packets or not matched cases, accept for now
     return nfq_set_verdict(qh, id, NF_ACCEPT, 0, NULL);
+}
+
+int main()
+{
+    struct nfq_handle *h;
+    struct nfq_q_handle *qh;
+    int fd;
+    int rv;
+    char buf[4096] __attribute__ ((aligned));
+
+    printf("Opening library handle\n");
+    h = nfq_open();
+    if (!h) {
+        fprintf(stderr, "Error during nfq_open()\n");
+        exit(1);
+    }
+
+    printf("Unbinding existing nf_queue handler for AF_INET (if any)\n");
+    if (nfq_unbind_pf(h, AF_INET) < 0) {
+        fprintf(stderr, "Error during nfq_unbind_pf()\n");
+        exit(1);
+    }
+
+    printf("Binding nfnetlink_queue as nf_queue handler for AF_INET\n");
+    if (nfq_bind_pf(h, AF_INET) < 0) {
+        fprintf(stderr, "Error during nfq_bind_pf()\n");
+        exit(1);
+    }
+
+    printf("Binding this socket to queue '0'\n");
+    qh = nfq_create_queue(h, 0, &cb, NULL);
+    if (!qh) {
+        fprintf(stderr, "Error during nfq_create_queue()\n");
+        exit(1);
+    }
+
+    printf("Setting copy_packet mode\n");
+    if (nfq_set_mode(qh, NFQNL_COPY_PACKET, 0xffff) < 0) {
+        fprintf(stderr, "Can't set packet_copy mode\n");
+        exit(1);
+    }
+
+    fd = nfq_fd(h);
+
+    while ((rv = recv(fd, buf, sizeof(buf), 0)) && rv >= 0) {
+        nfq_handle_packet(h, buf, rv);
+    }
+
+    printf("Unbinding from queue 0\n");
+    nfq_destroy_queue(qh);
+
+    printf("Closing library handle\n");
+    nfq_close(h);
+
+    return 0;
 }
